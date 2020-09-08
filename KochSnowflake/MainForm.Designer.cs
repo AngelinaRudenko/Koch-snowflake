483051a6555e5b705686a6994eee5ee88188bc03
AngelinaRudenko <angel.rudenko.777@gmail.com>
2020-09-09 00:32:27 +0300

    Initial commit

---
+++ KochSnowflake/MainForm.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace KochSnowflake
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButtonOutside = new System.Windows.Forms.RadioButton();
+            this.radioButtonInside = new System.Windows.Forms.RadioButton();
+            this.panel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDownIterations = new System.Windows.Forms.NumericUpDown();
+            this.panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownIterations)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radioButtonOutside
+            // 
+            this.radioButtonOutside.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.radioButtonOutside.AutoSize = true;
+            this.radioButtonOutside.ForeColor = System.Drawing.Color.Blue;
+            this.radioButtonOutside.Location = new System.Drawing.Point(39, 122);
+            this.radioButtonOutside.Name = "radioButtonOutside";
+            this.radioButtonOutside.Size = new System.Drawing.Size(89, 24);
+            this.radioButtonOutside.TabIndex = 0;
+            this.radioButtonOutside.Text = "Наружу";
+            this.radioButtonOutside.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonInside
+            // 
+            this.radioButtonInside.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.radioButtonInside.AutoSize = true;
+            this.radioButtonInside.Checked = true;
+            this.radioButtonInside.ForeColor = System.Drawing.Color.Blue;
+            this.radioButtonInside.Location = new System.Drawing.Point(39, 92);
+            this.radioButtonInside.Name = "radioButtonInside";
+            this.radioButtonInside.Size = new System.Drawing.Size(88, 24);
+            this.radioButtonInside.TabIndex = 1;
+            this.radioButtonInside.TabStop = true;
+            this.radioButtonInside.Text = "Внутрь";
+            this.radioButtonInside.UseVisualStyleBackColor = true;
+            // 
+            // panel
+            // 
+            this.panel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel.Controls.Add(this.label1);
+            this.panel.Controls.Add(this.numericUpDownIterations);
+            this.panel.Controls.Add(this.radioButtonInside);
+            this.panel.Controls.Add(this.radioButtonOutside);
+            this.panel.Location = new System.Drawing.Point(623, 12);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(175, 180);
+            this.panel.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Итерации";
+            // 
+            // numericUpDownIterations
+            // 
+            this.numericUpDownIterations.Location = new System.Drawing.Point(38, 53);
+            this.numericUpDownIterations.Maximum = new decimal(new int[] {
+            19,
+            0,
+            0,
+            0});
+            this.numericUpDownIterations.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownIterations.Name = "numericUpDownIterations";
+            this.numericUpDownIterations.Size = new System.Drawing.Size(90, 26);
+            this.numericUpDownIterations.TabIndex = 2;
+            this.numericUpDownIterations.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDownIterations.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ЛР 3 Снежинка Коха";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.panel.ResumeLayout(false);
+            this.panel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownIterations)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButtonOutside;
+        private System.Windows.Forms.RadioButton radioButtonInside;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDownIterations;
+    }
+}
+
---
+++ KochSnowflake/MainForm.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace KochSnowflake
+{
+    public partial class MainForm : Form
+    {
+        Graphics g;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Fractal(MyPoint A, MyPoint B, byte iterations)
+        {
+            if (iterations == 0)
+            {
+                MyPoint x = GetThirdPoint(A, B); //X - вершина будующего треугольника
+                DrawLine(A, x); //Рисуется /\
+                DrawLine(x, B);
+            }
+            else
+            {
+                iterations--;
+
+                MyPoint pt13AB = Get13Point(A, B); //Координата 1/3 отрезка AB
+                MyPoint pt23AB = Get23Point(A, B); //Координата 2/3 отрезка AB
+
+                DrawLine(A, pt13AB); //Рисуем
+                DrawLine(pt23AB, B); // _ _
+
+                if (iterations != 0) //Если предвидятся итерации, то происходит расчет координат
+                {
+                    MyPoint X = GetThirdPoint(pt13AB, pt23AB); //X - вершина будующего треугольника
+
+                    MyPoint pt13AX = Get13Point(pt13AB, X); //Координата 1/3 отрезка (от 1/3 AB до X)
+                    MyPoint pt23AX = Get23Point(pt13AB, X); //Координата 2/3 отрезка (от 1/3 AB до X)
+
+                    MyPoint pt13XB = Get13Point(pt23AB, X); //Координата 1/3 отрезка (от 2/3 AB до X)
+                    MyPoint pt23XB = Get23Point(pt23AB, X); //Координата 2/3 отрезка (от 2/3 AB до X)
+
+                    Fractal(pt13AB, X, iterations); //Тут происходит рекурсия, тем самым пускаются ветки
+                    Fractal(X, pt23AB, iterations);
+                }
+                else
+                {
+                    Fractal(pt13AB, pt23AB, iterations);
+                }
+            }
+        }
+
+        private void DrawLine(MyPoint pt1, MyPoint pt2) //Рисуем линию
+        {
+            g.DrawLine(new Pen(Color.GreenYellow), pt1.X, pt1.Y, pt2.X, pt2.Y);
+        }
+
+        private void DrawLine(Color penColor, Point pt1, Point pt2)
+        {
+            g.DrawLine(new Pen(penColor), pt1, pt2); //Рисуем линию своим цветом
+        }
+
+        private MyPoint GetThirdPoint(MyPoint A, MyPoint B) //Нахождение третьей точки равностороннего треугольника
+        {
+            double sixtyGraduses = Math.PI / 3;
+            double sixtyGradusesSin = Math.Sin(sixtyGraduses);
+            double sixtyGradusesCos = Math.Cos(sixtyGraduses);
+            MyPoint point = new MyPoint();
+            point.X = (float)((B.X - A.X) * sixtyGradusesCos - (B.Y - A.Y) * sixtyGradusesSin + A.X);
+            point.Y = (float)((B.X - A.X) * sixtyGradusesSin + (B.Y - A.Y) * sixtyGradusesCos + A.Y);
+            return point;
+        }
+
+        private MyPoint Get13Point(MyPoint A, MyPoint B) //Нахождение 1/3 отрезка
+        {
+            MyPoint point = new MyPoint(
+                            (2 * A.X + B.X) / 3,
+                            (2 * A.Y + B.Y) / 3);
+            return point;
+        }
+
+        private MyPoint Get23Point(MyPoint A, MyPoint B) //Нахождение 2/3 отрезка
+        {
+            MyPoint point = new MyPoint(
+                             (A.X + 2 * B.X) / 3,
+                             (A.Y + 2 * B.Y) / 3);
+            return point;
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            g = CreateGraphics();
+            g.Clear(Color.Black);
+            float x = 6 * MainForm.ActiveForm.Width / 10;
+            float y = MainForm.ActiveForm.Height / 10;
+            float x2 = 6 * MainForm.ActiveForm.Width / 10;
+            float y2 = 9 * (MainForm.ActiveForm.Height - 20) / 10;
+            MyPoint A = new MyPoint(x, y);
+            MyPoint B = new MyPoint(x2, y2);
+            MyPoint C = GetThirdPoint(A, B);
+            byte iterations = Convert.ToByte(numericUpDownIterations.Value);
+            if (radioButtonOutside.Checked)
+            {
+                Fractal(B, A, iterations);
+                Fractal(C, B, iterations);
+                Fractal(A, C, iterations);
+            }
+            else
+            {
+                Fractal(A, B, iterations);
+                Fractal(B, C, iterations);
+                Fractal(C, A, iterations);
+            }
+
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+
+            // Формат отображения содержимого TimeSpan
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            MessageBox.Show("Затраченное время: " + elapsedTime);
+        }
+    }
+
+    public class MyPoint
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+
+        public MyPoint(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+        public MyPoint() { }
+    }
+}
